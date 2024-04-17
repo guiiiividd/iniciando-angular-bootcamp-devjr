250bf4973c8548755e0b5377449c0b62cc3eef64
guiiiividd <guiiiividd@gmail.com>
2024-04-17 17:05:57 -0300

    :sparkles: feat: Criando Objetos a partir de uma  Interface

--- src/app/components/book/book.component.ts
+++ src/app/components/book/book.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Book } from '../../Book';
 
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
-  styleUrl: './book.component.css'
+  styleUrl: './book.component.css',
 })
 export class BookComponent {
-
+  book: Book = {
+    id: 1,
+    title: 'Conceitos Angular',
+    author: 'Guilherme Vidal',
+    price: 100,
+  };
 }
