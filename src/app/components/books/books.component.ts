de837845305ed2aa9afd32bf15ff107b2f3b8f96
guiiiividd <guiiiividd@gmail.com>
2024-04-23 13:43:13 -0300

    :sparkles: feat: Diretiva ngFor

---
+++ src/app/components/books/books.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { Book } from '../../Book';
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrl: './books.component.css',
+})
+export class BooksComponent {
+  books: Book[] = [
+    {
+      id: 1,
+      title: 'Código Limpo: Habilidades Práticas do Agile Software',
+      author: 'Robert C. Martin',
+      price: 95,
+    },
+    {
+      id: 2,
+      title: 'Conceitos Angular',
+      author: 'Guilherme Vidal',
+      price: 100,
+    },
+    {
+      id: 3,
+      title: 'O Programador Pragmático: De Aprendiz a Mestre',
+      author: 'Andrew Hunt e David Thomas',
+      price: 165,
+    },
+  ];
+}
